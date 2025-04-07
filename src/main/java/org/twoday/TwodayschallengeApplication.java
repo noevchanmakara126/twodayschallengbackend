2d01c9106401af62e4ff6c80297adb185b00186e
Noev Chanmakara <jrmakara97@gmail.com>
2025-04-07 13:50:51 +0700

    first commit :)

---
+++ src/main/java/org/twoday/TwodayschallengeApplication.java
@@ -0,0 +1,13 @@
+package org.twoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwodayschallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwodayschallengeApplication.class, args);
+    }
+
+}
